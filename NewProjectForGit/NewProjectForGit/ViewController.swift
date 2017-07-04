8ec2bdb5a57c77034a47087a2fd7a957183a91c1
Admin media <admin@ADMIN.local>
2017-07-04 13:07:32 +0530

    second commit

---
+++ NewProjectForGit/NewProjectForGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewProjectForGit
+//
+//  Created by Admin media on 7/4/17.
+//  Copyright © 2017 Media Mosaic service private limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
